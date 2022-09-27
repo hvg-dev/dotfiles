66273871756fdd4d1598d2ff4cc4d2974d6593d7
felegy <felegy@hvg.hu>
2022-09-27 19:25:20 +0200

    ~ FIX: Powershell Profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,29 +1,45 @@
 Switch([System.Diagnostics.Process]::GetCurrentProcess().ProcessName) {
     'pwsh' {
-        if (!(Get-Module -ListAvailable -Name PSReadLine)) 
+        if (!(Get-Module -ListAvailable -Name PSReadLine))
         {
-            Install-Module PSReadLine
+            Install-Module PSReadLine -Scope CurrentUser
         }
         Import-Module PSReadLine
-        if (!(Get-Module -ListAvailable -Name CompletionPredictor)) 
+        if (!(Get-Module -ListAvailable -Name CompletionPredictor))
         {
-            Install-Module CompletionPredictor
+            Install-Module CompletionPredictor -Scope CurrentUser
         }
         Import-Module CompletionPredictor
-        Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+        Set-PSReadLineOption -PredictionSource History
         Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
     }
     'powershell' {
-        # Running on Powershell
+        if (!(Get-Module -ListAvailable -Name posh-git))
+        {
+            Install-Module posh-git -Scope CurrentUser
+        }
+        Import-Module posh-git
     }
     default {
         ###
     }
 }
 
-if (!(Get-Module -ListAvailable -Name yarn-completion)) 
+if (!(Get-Module -ListAvailable -Name posh-docker))
 {
-    Install-Module yarn-completion
+    Install-Module posh-docker -Scope CurrentUser
+}
+Import-Module posh-docker
+
+if (!(Get-Module -ListAvailable -Name dockerComposeCompletion))
+{
+    Install-Module dockerComposeCompletion -Scope CurrentUser
+}
+Import-Module dockerComposeCompletion
+
+if (!(Get-Module -ListAvailable -Name yarn-completion))
+{
+    Install-Module yarn-completion -Scope CurrentUser
 }
 Import-Module yarn-completion
 
@@ -33,10 +49,11 @@ Set-Alias -Name d -Value docker
 
 Set-Alias -Name dc -Value docker-compose
 
-Function Admin-Terminal {
+Function Open-Admin-Terminal
+{
     Switch ( $Args.Count ) {
-        0 { 
-            Start-Process wt -Verb runAs 
+        0 {
+            Start-Process wt -Verb runAs
         }
         1 {
             Start-Process wt -Verb runAs -ArgumentList @("-d", $Args[0])
@@ -45,5 +62,5 @@ Function Admin-Terminal {
             Start-Process wt -Verb runAs -ArgumentList $Args
         }
     }
-}  
-Set-Alias -Name at -Value Admin-Terminal
+}
+Set-Alias -Name at -Value Open-Admin-Terminal
